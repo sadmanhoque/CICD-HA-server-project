01dbeea0a62b262596a9e245e31dcbdf33063654
unknown <sadmanhoque@yahoo.com>
2023-11-05 02:05:37 -0400

    working on adding a fargate profile for the cluster

--- EKS-cluster-deployer/main.tf
+++ EKS-cluster-deployer/main.tf
@@ -16,29 +16,40 @@ resource "aws_eks_cluster" "example" {
   ]
 }
 
-output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
-}
+resource "aws_eks_fargate_profile" "example" {
+  cluster_name           = aws_eks_cluster.example.name
+  fargate_profile_name   = "example"
+  pod_execution_role_arn = aws_iam_role.example.arn
+  subnet_ids = [aws_subnet.main.id, aws_subnet.second.id]
 
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
+  selector {
+    namespace = "example"
   }
+
+  depends_on = [
+    aws_iam_role.fargate-profile-example,
+    aws_iam_role_policy_attachment.example-AmazonEKSFargatePodExecutionRolePolicy
+  ]
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
+resource "aws_iam_role" "fargate-profile-example" {
+  name = "eks-fargate-profile-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.example.name
 }
 
 resource "aws_subnet" "main" {
@@ -93,4 +104,29 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.example.name
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
